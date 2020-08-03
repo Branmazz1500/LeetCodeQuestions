07cd462bbc15fee5d12be681b40fbe1fb584608b
brandonmazzarella <brandonmazzarella@192.168.0.170>
2020-08-03 10:36:22 -0700

    Added BinaryLinkedListConvert

--- src/easyQuestions/StringShuffler.java
+++ src/easyQuestions/StringShuffler.java
@@ -2,16 +2,18 @@ package easyQuestions;
 
 public class StringShuffler {
 	 public static String restoreString(String s, int[] indices) {
-	     
-		 StringBuilder sb = new StringBuilder();
-		 String result = "";
-		 for(int i = 0; i < indices.length; i++) {
-			 String letter = s.substring(indices[i],indices[i]+1);
-			 sb.append(letter);
-	     }
-		 return String.valueOf(sb);
-	    }
+	   char[] chars = new char[s.length()];
+	   for(int i = 0; i<indices.length; i++) {
+		   chars[indices[i]] = s.charAt(i);
+	   }
+	   return String.valueOf(chars);
 	 
+	 }
+	 /*
+	  * s = "aiohn", indices = [3,1,4,2,0]
+	  * [0,1,2,3,4]
+		Output: "nihao"
+	  */
 	 public static void main(String[] args) {
 		 int[] test = new int[] {3,1,0,2};
 		 System.out.println(restoreString("ABCD",test));
